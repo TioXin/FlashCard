dddbb8dbf9ca24e95591039f5f470eac41cc7751
TioXin <168221362+TioXin@users.noreply.github.com>
2024-11-06 08:20:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Rock',
+    'Vocalista Da Banda The Cure?',
+    'O Vocalista da banda se chama Robert Smith'
+)
+
+criaCartao(
+    'Rap',
+    'Quem é o cantor do albúm Graduation',
+    'O cantor se chama Kanye West'
+)
+
+criaCartao(
+    'Cultura',
+    'Qual é a motivação do surgimento da cultura punk no Brasil?',
+    'A ditadura militar'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o nome do fungo que come plástico?',
+    'O Nome é Parengyodontium album'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
